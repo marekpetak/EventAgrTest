8c075e75e8346ddad033dc9feb6711bd209aa7fc
Marek Petak <marekpetak@kannegiesser.com>
2015-11-13 10:19:59 +0000

    added client side event

--- EventAgrTest/Scripts/app.js
+++ EventAgrTest/Scripts/app.js
@@ -1,20 +1,39 @@
 ﻿(function () {
+
+    ClientSideEvent = function (message) {
+        this.message = message;
+    };
+
     'use strict';
 
     var app = angular.module("TestApp", ['signalR.eventAggregator']);
 
     app.controller("TestCtrl", ['$scope','$http', function ($scope, $http) {
 
+        $scope.count = 0;
+        $scope.message = "";
+        $scope.incommingMessage = "message in - empty";
+
         function onEvent(e) {
+
+            $scope.count++;
+
             console.log("event received => ", e);
         };
 
-        console.log("EventAgrTest.Events.MyMessage = " + EventAgrTest.Events.MyMessage);
+        function onClientEvent(message) {
+
+            $scope.incommingMessage = message;
+        };
 
         $scope.eventAggregator().subscribe(EventAgrTest.Events.MyMessage, onEvent);
+        $scope.eventAggregator().subscribe(ClientSideEvent, onClientEvent);
+        
+        $scope.sendMessage = function () {
 
-        console.log("2: " + EventAgrTest.Events.MyMessage);
+            $scope.eventAggregator().publish(new ClientSideEvent($scope.message));
+        };
 
-        $scope.headerText = "HEADER";
+        $scope.headerText = "SignalR.EventAggregatorProxy simple event test app";
     }]);
 })();
